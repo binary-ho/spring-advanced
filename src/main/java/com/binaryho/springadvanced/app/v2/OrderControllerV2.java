a0e5f18e4ab400e8f0b31f820a96e76b017148a6
binary_ho <dfghcvb11@naver.com>
2023-06-28 12:34:19 +0900

    feat : [섹션 1] 트랜잭션 ID와 level 반영된 HelloTraceV2 구현과 적용  (#1)

---
+++ src/main/java/com/binaryho/springadvanced/app/v2/OrderControllerV2.java
@@ -0,0 +1,31 @@
+package com.binaryho.springadvanced.app.v2;
+
+import com.binaryho.springadvanced.trace.TraceStatus;
+import com.binaryho.springadvanced.trace.hellotrace.HelloTraceV1;
+import com.binaryho.springadvanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    // MEMO : request string http://localhost:8080/v2/request?itemId=ex
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
