5941ed41137cd0978c5b54e8ffba8b8a4646cc0c
binary_ho <dfghcvb11@naver.com>
2023-07-01 15:09:35 +0900

    [섹션 4] 프록시 패턴과 데코레이터 패턴 #7 (#9)
    * feat : 섹션 4 proxy 프로젝트 모듈 의존성과 기본 파일 세팅 (#7)
    
    * feat : 프록시와 데코레이터 기본 레이어드 구조 v1, v2, v3 세팅 (#7)
    
    * feat : [중요] 프록시 패턴 접근제어 - 캐싱 (#7)
    
    * feat : 데코레이터 패턴 적용 (#7)
    
    * feat : 데코레이터 패턴 README 반영 (#7)

---
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,37 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.Component;
+import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.v1.MessageDecorator;
+import hello.proxy.pureproxy.decorator.code.v2.TimeDecorator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    @Test
+    void 아무_데코레이터도_적용하지_않은_상태() {
+        Component realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void MessageDecorator_를_통해_데코레이터_패턴_적용() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void TimeDecorator_를_통해_수행_시간을_측정할_수_있다() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
+}
